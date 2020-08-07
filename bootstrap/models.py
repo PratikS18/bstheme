5e1435c9682268b3e7ee886a7151b93bdd51d8df
Pratik Shet <packt.pratiks@gmail.com>
2020-08-07 13:32:42 +0000

    New updated files

--- bootstrap/models.py
+++ bootstrap/models.py
@@ -12,6 +12,9 @@ class TopList(models.Model):
     ISBN = models.CharField(max_length = 20, blank = False)
     Book_name = models.CharField(max_length = 150)
 
+    def __str__(self):
+        return self.Book_name
+
 class RawData(models.Model):
     ISBN = models.CharField(max_length = 20, blank = False)
     link = models.CharField(max_length=150)
